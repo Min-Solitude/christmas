a261c836a139a6ef108452ea432a927f8c03b367
Min solitude <121474260+Min-Solitude@users.noreply.github.com>
2023-12-19 17:52:30 +0700

    update

--- lucky-ball/asset/js/main.js
+++ lucky-ball/asset/js/main.js
@@ -37,6 +37,61 @@ var swiper4 = new Swiper(".swiper-bag-content", {
   },
 });
 
+const dataReward = [
+  {
+    text: "A",
+    url: "/asset/img/qua/A.png",
+  },
+  {
+    url: "/asset/img/qua/C.png",
+    text: "C",
+  },
+  {
+    url: "/asset/img/qua/E.png",
+    text: "E",
+  },
+  {
+    url: "/asset/img/qua/H.png",
+    text: "H",
+  },
+  {
+    url: "/asset/img/qua/I.png",
+    text: "I",
+  },
+  {
+    url: "/asset/img/qua/M.png",
+    text: "M",
+  },
+  {
+    url: "/asset/img/qua/N.png",
+    text: "N",
+  },
+  {
+    url: "/asset/img/qua/P.png",
+    text: "P",
+  },
+  {
+    url: "/asset/img/qua/R.png",
+    text: "R",
+  },
+  {
+    url: "/asset/img/qua/S.png",
+    text: "S",
+  },
+  {
+    url: "/asset/img/qua/T.png",
+    text: "T",
+  },
+  {
+    url: "/asset/img/qua/W.png",
+    text: "W",
+  },
+  {
+    url: "/asset/img/qua/Y.png",
+    text: "Y",
+  },
+];
+
 // OPENING
 const SCREEN = document.querySelector("#pick-ball");
 const AWARD_ANIMATE_ELEMENT = document.querySelector("#award-animate");
@@ -52,11 +107,18 @@ const handleOpening = () => {
   setTimeout(() => {
     // random award to replace call api
     const finalAward = Math.floor(Math.random() * 1000);
-    displayAmountAward(finalAward);
+    // displayAmountAward(finalAward);
 
     // call success
     AWARD_ANIMATE_ELEMENT.innerHTML = `
-          <img src="/asset/gif/Snowflake.gif" alt="">
+          <p class='size_text_award'>
+            Congratulation! You've received the letter "${
+              dataReward[finalAward % 13].text
+            }"
+          </p>
+          <img class='qua_awrad' style={{width: 2rem}} src="${
+            dataReward[finalAward % 13].url
+          }" alt="award" />
       `;
     SCREEN.classList.add("opened");
   }, 1000);
